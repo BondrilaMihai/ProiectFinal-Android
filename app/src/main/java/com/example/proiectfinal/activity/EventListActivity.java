7e97b1f095e93626343589929db65de2b5a51717
BondrilaMihai <Bondrila@protonmail.com>
2020-04-28 18:37:39 +0300

    add facebook api

--- app/src/main/java/com/example/proiectfinal/activity/EventListActivity.java
+++ app/src/main/java/com/example/proiectfinal/activity/EventListActivity.java
@@ -2,12 +2,15 @@ package com.example.proiectfinal.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.proiectfinal.R;
 import com.example.proiectfinal.adapter.EventAdapter;
 import com.example.proiectfinal.model.Event;
@@ -15,9 +18,14 @@ import com.example.proiectfinal.model.Event;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class EventListActivity extends Activity {
 
     ArrayList<Event> events;
+
+    CircleImageView circleImageView;
+    TextView name;
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -29,6 +37,21 @@ public class EventListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        circleImageView = findViewById(R.id.profile_image);
+        name = findViewById(R.id.profile_name);
+
+
+        String facebookName = getIntent().getExtras().getString("name");
+        String facebookProfileImage = getIntent().getExtras().getString("image");
+
+        name.setText(facebookName);
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.dontAnimate();
+
+        Glide.with(EventListActivity.this).load(facebookProfileImage).into(circleImageView);
+
+
         recyclerView = (RecyclerView) findViewById(R.id.rvEvents);
 
         recyclerView.setHasFixedSize(true);
--- app/src/main/java/com/example/proiectfinal/activity/UserLoginActivity.java
+++ app/src/main/java/com/example/proiectfinal/activity/UserLoginActivity.java
@@ -9,16 +9,41 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 import com.example.proiectfinal.R;
 import com.example.proiectfinal.database.DatabaseHelper;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserLoginActivity  extends Activity {
     EditText username, password;
     Button login, register;
+    LoginButton  loginWithFacebook;
+
+    CircleImageView circleImageView;
+    TextView name, email;
 
     DatabaseHelper myDb;
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +55,28 @@ public class UserLoginActivity  extends Activity {
         login = (Button)findViewById(R.id.login);
         register = (Button)findViewById(R.id.goToRegister);
 
+        loginWithFacebook = (LoginButton)findViewById(R.id.loginWithFacebook);
+
         username = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.password);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginWithFacebook.setReadPermissions(Arrays.asList("email", "public_profile"));
+        loginWithFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+            }
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+            }
+        });
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,7 +88,7 @@ public class UserLoginActivity  extends Activity {
                 if(res.getCount() > 0) {
                     Toast.makeText(getApplicationContext(),
                             "Redirecting...",Toast.LENGTH_SHORT).show();
-                    loginUser();
+                    loginUser(stringUsername,"@mipmap/ic_launcher");
                 } else {
                     Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
                 }
@@ -59,8 +104,53 @@ public class UserLoginActivity  extends Activity {
     }
 
 
-    private void loginUser() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if(currentAccessToken == null) {
+                //todo: redirectionare catre login
+            } else {
+                loadUserProfile(currentAccessToken);
+            }
+        }
+    };
+
+    private void loadUserProfile(AccessToken newAccesToken) {
+        GraphRequest request = GraphRequest.newMeRequest(newAccesToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String id = object.getString("id");
+                    String image_url = "https://graph.facebook.com/"+id+"/picture?type=normal";
+
+
+
+                    loginUser(first_name + " " + last_name , image_url);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name,last_name,email,id");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    private void loginUser(String name, String image) {
         Intent intent = new Intent(this, EventListActivity.class);
+        intent.putExtra("name", name);
+        intent.putExtra("image", image);
         startActivity(intent);
     }
 
